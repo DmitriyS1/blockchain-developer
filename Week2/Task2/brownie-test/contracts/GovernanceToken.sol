a7392bc17639e7da1cbe734c15234b8a404e7cd6
Dmitriy Semenov <dmitriy.a.semenov@outlook.com>
2022-05-05 14:07:38 +0300

    Update GovernanceToken

--- Week2/Task2/brownie-test/contracts/GovernanceToken.sol
+++ Week2/Task2/brownie-test/contracts/GovernanceToken.sol
@@ -4,10 +4,6 @@ pragma solidity 0.6.12;
 
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-// import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-// import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-// import "OpenZeppelin/contracts/access/Ownable.sol";
-// import "@openzeppelin/contracts/utils/math/Math.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
 
@@ -20,9 +16,9 @@ contract GovernanceToken {
     }
 
     mapping (uint => Candidate) private candidates;
-    uint256 private fullBalance ;
+    uint256 private fullBalance ; // Будет ли балансе храниться все время как и кол-во кандидатов и сами кандидаты?
 
-    uint public candidatesCount;
+    uint private candidatesCount;
 
     constructor() public {
         candidatesCount = 0;
@@ -33,23 +29,36 @@ contract GovernanceToken {
     uint256 public decimals;
     uint256 public totalSupply;
 
+    // payable function where ether is coming
+    function deposit() public payable {
+        shareIncome(msg.value);
+    }
+
+    // share coming ether between candidates
+    function shareIncome(uint value) private {
+        for (uint i = 0; i < candidatesCount; i++){
+            transfer(candidates[i].candidateAddress, value * candidate[i].portion);
+        }
+
+        fullBalance += value;
+    }
+
+    // transfer ether to particular address
+    function transfer(address _to, uint _amount) {
+        // Как произвести трансфер на конкретный адрес?
+    }
+
+    // Add new candidate to the candidates set
     function addCandidate(address _candidateAddress, uint256 _balance) public {
+        candidate = Candidate(msg.sender, msg.value, msg.value / fullBalance); // ?
         require(_balance > 0);
         fullBalance += _balance;
         for (uint i = 0; i < candidatesCount; i++) {
             candidates[i].portion = candidates[i].balance / fullBalance;
         }
-
-        candidates.push(Candidate(_candidateAddress, _balance / fullBalance));
+        
         candidatesCount++;
-    }
-
-    function pay(uint value) public {
-        require(candidatesCount > 0);
-        for (uint i = 0; i < candidatesCount; i++) {
-            candidates[i].candidateAddress.transfer(value * candidates[i].portion / 100);
-        }
-
-        fullBalance += value;
+        candidates[candidatesCount] = Candidate(_candidateAddress, msg. / fullBalance);
+        candidatesCount++;
     }
 }
