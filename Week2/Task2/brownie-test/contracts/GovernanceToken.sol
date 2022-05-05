796e0e49c133d1c8d1554752bae625a68f950318
Dmitriy Semenov <dmitriy.a.semenov@outlook.com>
2022-05-05 19:46:03 +0300

    Updated readme; Added contracts

--- Week2/Task2/brownie-test/contracts/GovernanceToken.sol
+++ Week2/Task2/brownie-test/contracts/GovernanceToken.sol
@@ -6,7 +6,7 @@ pragma solidity 0.6.12;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
-
+// Как можно доработать и в чем ошибка выбранного пути?
 contract GovernanceToken {
     using SafeMath for uint256;
     struct Candidate {
@@ -16,7 +16,7 @@ contract GovernanceToken {
     }
 
     mapping (uint => Candidate) private candidates;
-    uint256 private fullBalance ; // Будет ли балансе храниться все время как и кол-во кандидатов и сами кандидаты?
+    uint256 private fullBalance ; // Будет ли баланс храниться все время как и кол-во кандидатов и сами кандидаты?
 
     uint private candidatesCount;
 
@@ -45,20 +45,19 @@ contract GovernanceToken {
 
     // transfer ether to particular address
     function transfer(address _to, uint _amount) {
-        _to.transfer(_amount);
+        _to.transfer(_amount); // Правильно ли сделан трансфер на другой аккаунт?
     }
 
     // Add new candidate to the candidates set
     function addCandidate(address _candidateAddress, uint256 _balance) public {
-        candidate = Candidate(msg.sender, msg.value, msg.value / fullBalance); // ?
+        candidate = Candidate(msg.sender, msg.value, msg.sender.balance / fullBalance); // Как должна работать арифметика если все числа целочисленные?
         require(_balance > 0);
         fullBalance += _balance;
-        for (uint i = 0; i < candidatesCount; i++) {
+        for (uint i = 0; i < candidatesCount; i++) { // Можно ли избегать цикла? Ведь за каждое действие платится газ
             candidates[i].portion = candidates[i].balance / fullBalance;
         }
         
         candidatesCount++;
-        candidates[candidatesCount] = Candidate(_candidateAddress, msg. / fullBalance);
-        candidatesCount++;
+        candidates[candidatesCount] = candidate;
     }
 }
