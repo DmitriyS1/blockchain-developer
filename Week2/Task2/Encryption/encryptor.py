8c0052f750c6ee62606125e1ad146860005f0a7c
Dmitriy Semenov <dmitriy.a.semenov@outlook.com>
2022-05-07 18:14:29 +0300

    Fixed encryptor

--- Week2/Task2/Encryption/encryptor.py
+++ Week2/Task2/Encryption/encryptor.py
@@ -1,28 +1,32 @@
+from datetime import datetime, tzinfo
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
-from base64 import b64decode
+from base64 import b64decode, b64encode
 import binascii
 
 
-def encrypt(message) -> bytes:
+def encrypt(message: str) -> bytes:
     public_key_raw_str = input("Insert your public key:")
     decoded_pub_key = b64decode(public_key_raw_str)
     public_key = RSA.import_key(decoded_pub_key)
 
-    msg = message.encode("ascii")
+    #msg = b64decode(message)
 
     encryptor = PKCS1_OAEP.new(public_key)
-    encrypted = encryptor.encrypt(msg)
-    print("Encrypted:", binascii.hexlify(encrypted))
-    return encrypted.decode("ascii")
+    encrypted = encryptor.encrypt(message.encode())
+    # with open("encrypted_.txt", "wb") as f:
+    #     f.write(encrypted)
+    print(binascii.hexlify(encrypted))
+    return encrypted
 
 
 def decrypt(message) -> bytes:
     private_key_raw_str = input("Insert your private key:")
     decoded_priv_key = b64decode(private_key_raw_str)
     private_key = RSA.import_key(decoded_priv_key)
-    
+    text = binascii.a2b_hex(message)
+
     decryptor = PKCS1_OAEP.new(private_key)
-    decrypted = decryptor.decrypt(message)
+    decrypted = decryptor.decrypt(text)
     print('Decrypted:', decrypted)
     return decrypted
--- Week2/Task2/Encryption/main.py
+++ Week2/Task2/Encryption/main.py
@@ -6,10 +6,10 @@ def main():
     if input_str == "1":
         key_creator.generate_keys()
     elif input_str == "2":
-        message = input("Insert your message:")
+        message = input("Insert adresses in next format:\n\"0x0000000000000000000000000000000000000001\", \"0x0000000000000000000000000000000000000002\", \"0x0000000000000000000000000000000000000003\"\n")
         encrypted = encryptor.encrypt(message)
     elif input_str == "3":
-        message = input("Insert your encrypted message:").encode()
+        message = input("Insert your encrypted message:")
         decrypted = encryptor.decrypt(message)
     elif input_str == "quit":
         print("Bye!")
