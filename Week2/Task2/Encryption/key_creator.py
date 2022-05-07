b9c57fd30cd07e1eb0fb1b70bf0a2ee14492966b
Dmitriy Semenov <dmitriy.a.semenov@outlook.com>
2022-05-07 12:53:08 +0300

    Created decryptor and encryptor (not working)

---
+++ Week2/Task2/Encryption/key_creator.py
@@ -0,0 +1,15 @@
+from Crypto.PublicKey import RSA
+
+def generate_keys():
+    keyPair = RSA.generate(3072)
+
+    pubKey = keyPair.publickey()
+    # print(f"Public key:  (n={hex(pubKey.n)}, e={hex(pubKey.e)})")
+    pubKeyPEM = pubKey.exportKey()
+    print("Save keys in safe place.\nPUBLIC_KEY:")
+    print(pubKeyPEM.decode('ascii'))
+
+    # print(f"Private key: (n={hex(pubKey.n)}, d={hex(keyPair.d)})")
+    privKeyPEM = keyPair.exportKey()
+    print("PRIVATE_KEY:")
+    print(privKeyPEM.decode('ascii'))
