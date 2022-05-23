44f5f833b988ae29ffb07a42237ac547b18cbfb0
Dmitriy Semenov <dmitriy.a.semenov@outlook.com>
2022-05-23 14:34:11 +0300

    Added scoring api

---
+++ Week5/ETHScoring/ETHScoring/Controllers/ScoringController.cs
@@ -0,0 +1,70 @@
+﻿using ETHScoring.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using System.Text.Json;
+
+namespace ETHScoring.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScoringController : ControllerBase
+    {
+        private readonly ILogger<ScoringController> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        private const string ETHERSCAN_ADDRESS = "https://api.etherscan.io/api";
+
+        public ScoringController(
+            ILogger<ScoringController> logger,
+            IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ScoreAsync(
+            [FromRoute] string address, 
+            [FromQuery] string shopAddress,
+            CancellationToken ct = default)
+        {
+            var transactions = await GetTransactions(address, ct);
+        }
+
+        private async Task<List<TransactionItem>> GetTransactions(string address, CancellationToken ct)
+        {
+            var requestAddress = $@"{ETHERSCAN_ADDRESS}?module=account
+   &action=txlist
+   &address={address}
+   &startblock=11967948
+   &endblock=99999999
+   &page=1
+   &offset=100
+   &sort=desc
+   &apikey={444}";
+
+            var request = new HttpRequestMessage(
+                HttpMethod.Get,
+                requestAddress)
+            {
+                Headers =
+                {
+                    { HeaderNames.UserAgent, "" }
+                }
+            };
+
+            var httpClient = _httpClientFactory.CreateClient();
+            var response = await httpClient.SendAsync(request, ct);
+            var transactions = new List<TransactionItem>();
+            if (response.IsSuccessStatusCode)
+            {
+                using var contentStream = await response.Content.ReadAsStreamAsync();
+                var transactionsResponse = await JsonSerializer.DeserializeAsync<TransactionResponse>(contentStream);
+                transactions = transactionsResponse.Result;
+            }
+
+            return transactions;
+        }
+    }
+}
---
+++ Week5/ETHScoring/ETHScoring/Models/TransactionResponse.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ETHScoring.Models
+{
+    public class TransactionItem
+    {
+        public uint BlockNumber { get; set; }
+
+        public uint TimeStamp { get; set; }
+
+        public string Hash { get; set; }
+
+        public uint Nonce { get; set; }
+
+        public string BlockHash { get; set; }
+
+        public string TransactionIndex { get; set; }
+
+        public string From { get; set; }
+
+        public string To { get; set; }
+
+        public string Value { get; set; }
+
+        public uint Gas { get; set; }
+
+        public uint GasPrice { get; set; }
+
+        public uint IsError { get; set; }
+
+        [JsonPropertyName("txreceipt_status")]
+        public string TxReceiptStatus { get; set; }
+
+        public string Input { get; set; }
+
+        public string ContractAddress { get; set; }
+
+        public string CumulativeGasUsed { get; set; }
+
+        public string GasUsed { get; set; }
+
+        public uint Confirmations { get; set; }
+    }
+
+    public class TransactionResponse {
+        public string Status { get; set; }
+
+        public string Message { get; set; }
+
+        public List<TransactionItem> Result { get; set; }
+    }
+}
